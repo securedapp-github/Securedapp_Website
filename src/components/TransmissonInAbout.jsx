1019e620a6bf64cfe7c47101ea7fbef5e376adce
himang305 <himang305@gmail.com>
2023-08-05 22:00:23 +0530

    add_scanComp_to_Flatcontract

--- src/components/TransmissonInAbout.jsx
+++ src/components/TransmissonInAbout.jsx
@@ -14,10 +14,10 @@ const TransmissionInAbout = () => {
       </div>
       <div className="py-8">
         <button className="email-button rounded-3xl bg-gray-500 text-white  flex items-center justify-between md:w-[620px] md:h-[92px] w-[300px]  py-7 px-9 ">
-          <span class="email-text font-light font-sans text-2xl leading-[110%]">
+          <span className="email-text font-light font-sans text-2xl leading-[110%]">
             Your email
           </span>
-          <span class="arrow font-light font-sans text-2xl leading-[110%]">
+          <span className="arrow font-light font-sans text-2xl leading-[110%]">
             &gt;
           </span>
         </button>
