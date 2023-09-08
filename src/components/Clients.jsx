3ec8cc1626d609b9fb7fb0f19e02c566250d89a7
manvideore21 <137396100+manvideore21@users.noreply.github.com>
2023-09-08 12:21:13 +0530

    Update Clients.jsx
    added marquee tag in clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,24 +1,50 @@
 import React from "react";
 import SectionHeader from "./SectionHeader";
+import Marquee from "react-fast-marquee"
+import logo1 from "../logo1.png"
+import logo2 from "../logo2.png"
+import logo3 from "../logo3.png"
+import logo4 from "../logo4.png"
+import logo5 from "../logo5.png"
+
 import clientLogo from "../clientlogos.png";
 
 const Clients = () => {
   return (
-    <div className="client  md:p-[100px] lg:pt-[100px] md:pt-[300px]  lg:p-[80px] p-[50px] md:px-[80px] flex lg:flex-row md:flex-col flex-col ">
+    <>
+    <div className="client  md:p-[800px] md:pt-[20px] lg:p-[80px] p-[50px] md:px-[80px] flex lg:flex-row md:flex-col flex-col ">
       <div className="lg:w-4/12 mr-[50px]">
         <SectionHeader content={"Client"} />
         <div className="text-white pt-1 md:text-[20px] text-[18px] mt-[20px] font-normal font-sans leading-[135%] text-opacity-60 ">
           Leading DApp startups trust us to secure their blockchain applications
         </div>
       </div>
-      <div className="flex flex-wrap lg:px-[30px] lg:ml-[60px] lg:mt-0 mt-[50px] ">
-        <img
-          src={clientLogo}
-          alt="logos"
-          className="md:w-auto md:h-auto w-[600px] h-[100px]"
-        />
-      </div>
-    </div>
+      
+     
+      <Marquee>
+  <div>
+    <img src={logo1} alt="logo1" className="w-50 h-16" />
+  </div>
+  <div>
+    <img src={logo2} alt="logo2" className="w-50 h-16" />
+  </div>
+  <div>
+    <img src={logo3} alt="logo3" className="w-50 h-16" />
+  </div>
+  <div>
+    <img src={logo4} alt="logo4" className="w-50 h-16"/>
+  </div>
+  <div>
+    <img src={logo5} alt="logo5" className="w-50 h-16" />
+  </div>
+</Marquee>
+
+
+     </div>
+    
+
+    </>
+    
   );
 };
 
