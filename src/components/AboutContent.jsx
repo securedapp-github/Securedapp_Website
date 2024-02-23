e027ba6d1ae31cb06c2e1a1267e25dc4b9af8a61
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-02-23 17:12:03 +0530

    Changes In UI Done By Kunal Khare - 23/02/2024

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const AboutContent = ({ content,check }) => {
   return (
     <>
-      <p className={`text-white font-sans text-[${check?'20px':'16px'}] font-${check?'medium':'normal'} leading-[150%] `}>
-        {content}
-      </p>
+      <p className={`text-white font-sans ${check ? 'text-20' : 'text-16'} ${check ? 'font-medium' : 'font-normal'} leading-[150%]`}>
+  {content}
+</p>
+
       <br />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
-      gilroy:['Gilroy']
+      gilroy:["Gilroy", "sans-serif"]
     },
     extend: {
       colors: {
