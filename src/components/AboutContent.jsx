62345f5ea6e796b93dd1fb5dc5778a3c1d7dd066
himang305 <himang305@gmail.com>
2024-02-25 20:17:43 +0530

    fix

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AboutContent = ({ content }) => {
+const AboutContent = ({ content,check }) => {
   return (
     <>
       <p className={`text-white font-sans ${check ? 'text-20' : 'text-16'} ${check ? 'font-medium' : 'font-normal'} leading-[150%]`}>
