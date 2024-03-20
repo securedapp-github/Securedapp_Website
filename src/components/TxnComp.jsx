e20f6df570fc3459c7adefb31361cd2ecd64e6bd
Vedant Khatri <98382732+LuciferVedant@users.noreply.github.com>
2024-03-20 20:06:21 +0530

    Added Transaction Success and Failure Page

--- src/components/TxnComp.jsx
+++ src/components/TxnComp.jsx
@@ -3,19 +3,19 @@ import NavbarWithBread from "./NavWithBread";
 import ContactHero from "./ContactHero";
 import Transmission from "./Transmission";
 import FooterInAbout from "./FooterInAbout";
+import SuccessImage from "../images/successful.png";
+import TransactionPendingImage from "../images/transaction-history.png";
 
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "utils/loader";
-import { useParams } from 'react-router-dom';
-
+import { useParams } from "react-router-dom";
 
 const TxnHero = () => {
   const [status, setstatus] = useState("");
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
-
 
   const blurryDivStyle = {
     filter: loading ? "blur(5px)" : "blur(0px)",
@@ -23,7 +23,7 @@ const TxnHero = () => {
 
   const fetchTxnData = async () => {
     try {
-      const apiUrl = 'https://139-59-5-56.nip.io:3443/check-phonepay';
+      const apiUrl = "https://139-59-5-56.nip.io:3443/check-phonepay";
       const response = await fetch(`${apiUrl}?txnid=${id}`);
       const result = await response.json();
 
@@ -31,10 +31,9 @@ const TxnHero = () => {
       setLoading(false);
       if (result.data?.status && result.data.status === 1) {
         setstatus("Successful");
-      }else{
+      } else {
         setstatus("Pending (Contact Team)");
       }
-
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -44,8 +43,6 @@ const TxnHero = () => {
   useEffect(() => {
     fetchTxnData();
   }, []);
-
-
 
   return (
     <>
@@ -60,12 +57,41 @@ const TxnHero = () => {
         style={{ ...blurryDivStyle }}
         className="contacthero md:pt-[80px] pt-[130px]"
       >
-        <div className=" lg:pt-[110px] pt-[10px] py-[60px]  md:px-[80px]  md:py-[50px] lg:pb-[60px] py-[50px]  w-full mb-[0px] flex lg:flex-row flex-col gap-[60px]">
-            {!loading && (
-              <h1 className="text-white text-[36px] font-normal font-sans leading-[110%] pb-[20px]">
-              Transaction Status : {status}
-              </h1>
-            )}
+        <div className="sm:p-[50px] lg:p-[70px]">
+          {!loading && (
+            <div className="text-center text-white">
+              <img
+                className="mx-auto mb-4" // Center align the image horizontally
+                src={
+                  status === "Successful"
+                    ? SuccessImage
+                    : TransactionPendingImage
+                }
+                height={300}
+                width={300}
+                alt=""
+              />
+              {status === "Successful" ? (
+                <>
+                  <div className="text-[50px]">Thank you.</div>
+                  <div className="text-[35px] pb-2">
+                    Payment Done Successfully!
+                  </div>
+                  <div className="text-[25px]">
+                    You will be redirected to the home page shortly or else
+                    click below to go to the Home page.
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="text-[50px] pb-2">Pending</div>
+                  <div className="text-[25px]">
+                    Your Transaction Status is {status}
+                  </div>
+                </>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </>
